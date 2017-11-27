c95da3412456411577c472a334bc6b9388edf9cd
Davide Gomba <davide.gomba@gmail.com>
2017-11-27 09:40:40 +0100

    updating documentation

---
+++ HA-Wemos-Client-Relay-Switch.ino/HA-Wemos-Client-Relay-Switch.ino.ino
@@ -0,0 +1,161 @@
+/*
+  Basic ESP8266 MQTT example
+
+  This sketch demonstrates the capabilities of the pubsub library in combination
+  with the ESP8266 board/library.
+
+  It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic" every two seconds
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
+    else switch it off
+
+  It will reconnect to the server if the connection is lost using a blocking
+  reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+  achieve the same result without blocking the main loop.
+
+  To install the ESP8266 board, (using Arduino 1.6.4+):
+  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
+       http://arduino.esp8266.com/stable/package_esp8266com_index.json
+  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
+  - Select your ESP8266 in "Tools -> Board"
+
+
+  This code talks to an Home Assistant Server hosted on a custom ip address. 
+  This is the config.yaml that it talks to:
+
+  switch:
+  - platform: mqtt
+    name: "Davide"
+    state_topic: "status" # lo rimandi tu quando non sei in optimistic
+    command_topic: "relay/set"
+    availability_topic: "relay/set/available"
+    payload_on: "1"
+    payload_off: "0"
+    optimistic: true
+    qos: 0
+    retain: true
+
+
+  This is the view section in order to see it
+
+  group:
+  default_view:
+    view: yes
+    entities:
+      - switch.Davide
+
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
+const char* mqtt_server = "IP ADDRESS";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+const int relayPin = D1;
+const long interval = 2000;  // pause for two seconds
+
+
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  pinMode(relayPin, OUTPUT);
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    Serial.println("Payload received");
+    client.publish("relay/set/status", "1");
+    digitalWrite(relayPin, HIGH);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+    delay(1000);
+  } else if((char)payload[0] == '0'){
+    digitalWrite(relayPin, LOW);  // Turn the LED off by making the voltage HIGH
+    client.publish("relay/set/status", "0");
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+    //  client.publish("sensorbox/sensorbox/gas", "hello world");
+      // ... and resubscribe
+      client.subscribe("relay/set");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, 75, "", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("sensorbox/sensorbox/gas", msg);
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# talk-to-your-home-basic-workshop
+## Talk To Your Home Basic Workshop
+
+The **"Talk To Your Home Basic Workshop "** is an explorative 12-15 hours workshop made by Davide Gomba and the Casa Jasmina Team in late Autumn 2017, aimed to create the referral documentation to the creation of a domotic home based on the student needs.
+
+The setup features:
+
+* Basic [IFTT](https://ifttt.com/) _applets_ or [Zapier](https://zapier.com/)'s' _zaps_
+* [Blynk](http://www.blynk.cc/) tests
+* a Basic [Home Assistant](https://home-assistant.io) Setup based on custom or OEM components, such as Arduino MKR1000 or Wemos D1.
+
+Slides are [here](https://docs.google.com/presentation/d/16lk0KtPRGYOY2uQPkaSlnNrjV0hy635jm_0IGNkW4O4/edit#slide=id.g2ae3e234c7_0_0) [1].
+
+
+
+
+[1] If you can't access them, it means you are not attending the workshop. Don't worry we'll manage to release this in the future
